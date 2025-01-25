bf63e9c09aded550c342cdf5e67b61a327fe1fa1
Sahara Sipple <sahara36@usf.edu>
2025-01-25 11:44:32 -0500

    Task 1 -  Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+// Task 1: Working with Arrays //
+
+let products = ["Pen", "Pencil", "Marker", "Highlighter", "Eraser"]; // declare the array "products" which contains pen, pencil, marker, highlighter, and eraser 
+products.unshift("Crayon"); // adds "crayon" to the beginning of the array
+let popped = products.pop(); // establishes "Eraser" as the item to be deleted from the end of the array
+console.log(products); // Displays the array as Crayon, Pen, Pencil, Marker, Highlighter
